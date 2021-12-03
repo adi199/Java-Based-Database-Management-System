6461f00f460fdeb8993784b52ed0af513e746590
NiraliPPatel <95332188+NiraliPPatel@users.noreply.github.com>
2021-12-03 14:52:09 -0600

    Updated variable names and comments

--- src/main/java/edu/blue/extensions/helpers/DropOperationHelper.java
+++ src/main/java/edu/blue/extensions/helpers/DropOperationHelper.java
@@ -9,55 +9,55 @@ import java.util.Arrays;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-import static edu.blue.extensions.helpers.DeleteOperationHelper.performDeleteOperation;
+import static edu.blue.extensions.helpers.DeleteOperationHelper.handleDeleteOperation;
 
 /**
- * This helper facade is used for performing drop operation on the table and implicitly drops the indexes if
+ * This helper facade is used to perform drop operation on the table and implicitly drops the indexes if
  * any of them are specified on the tables columns
  *
- * @author Team Blue
+ * @author Team Ottawa
  */
 public class DropOperationHelper {
 
-    private static final Logger LOGGER = Logger.getLogger(DropOperationHelper.class.getName());
+    private static final Logger logger = Logger.getLogger(DropOperationHelper.class.getName());
 
     /**
-     * This method performs the record deletion operation.
+     * This method used to perform the record deletion operation.
      *
-     * @param dropTableString
+     * @param dropTableQueryString
      */
-    public static void dropTable(String dropTableString) {
+    public static void handleDropTableOperation(String dropTableQueryString) {
         try {
-            String[] tokens = dropTableString.split(" ");
+            String[] tokens = dropTableQueryString.split(" ");
             if (!(tokens[0].trim().equalsIgnoreCase("DROP") && tokens[1].trim().equalsIgnoreCase("TABLE"))) {
-                System.out.println("Error");
+                System.out.println("Invalid Syntax!");
                 return;
             }
 
-            ArrayList<String> dropTableTokens = new ArrayList<>(Arrays.asList(dropTableString.split(" ")));
+            ArrayList<String> dropTableTokens = new ArrayList<>(Arrays.asList(dropTableQueryString.split(" ")));
             String tableName = dropTableTokens.get(2);
 
 
-            performDeleteOperation("delete from table " + DBOperationsProcessor.tablesTable + " where table_name = '" + tableName + "' ");
-            performDeleteOperation("delete from table " + DBOperationsProcessor.columnsTable + " where table_name = '" + tableName + "' ");
+            handleDeleteOperation("delete from table " + DBOperationsProcessor.tablesTable + " where table_name = '" + tableName + "' ");
+            handleDeleteOperation("delete from table " + DBOperationsProcessor.columnsTable + " where table_name = '" + tableName + "' ");
             File tableFile = new File(Utils.getTableFilePath(tableName));
             if (tableFile.delete()) {
-                System.out.println("Table " + tableName + " dropped successfully.");
+                System.out.println(tableName + " - Table dropped successfully.");
             } else System.out.println("Table doesn't exist!");
 
 
-            File f = new File("data/user_data/");
-            File[] matchingFiles = f.listFiles((dir, name) -> name.startsWith(tableName) && name.endsWith("ndx"));
-            boolean flag = false;
-            for (File file : matchingFiles) {
-                if (file.delete()) {
-                    flag = true;
+            File file = new File("data/user_data/");
+            File[] matchingFiles = file.listFiles((dir, name) -> name.startsWith(tableName) && name.endsWith("ndx"));
+            boolean deleteFlag = false;
+            for (File fileRecord : matchingFiles) {
+                if (fileRecord.delete()) {
+                    deleteFlag = true;
                 }
             }
-            if (flag)
-                System.out.println("Dropped indexes");
+            if (deleteFlag)
+                System.out.println("Indexes dropped successfully");
         } catch (Exception e) {
-            LOGGER.log(Level.SEVERE, "Failed dropping table ", e);
+            logger.log(Level.SEVERE, "Failed dropping table ", e);
         }
     }
 
