23c04839090491f98ba77deca34eb03b08283e53
NiraliPPatel <95332188+NiraliPPatel@users.noreply.github.com>
2021-12-03 14:53:40 -0600

    Updated variable names and comments

--- src/main/java/edu/blue/extensions/helpers/SelectOperationHelper.java
+++ src/main/java/edu/blue/extensions/helpers/SelectOperationHelper.java
@@ -16,78 +16,78 @@ import static edu.blue.Utils.getConditionFromCurrentQuery;
 import static edu.blue.Utils.getTableFilePath;
 
 /**
- * This helper class is used for performing select operations
+ * This helper class is used to handle select operations
  *
- * @author Team Blue
+ * @author Team Ottawa
  */
 public class SelectOperationHelper {
 
-    private static final Logger LOGGER = Logger.getLogger(SelectOperationHelper.class.getName());
+    private static final Logger logger = Logger.getLogger(SelectOperationHelper.class.getName());
 
     /**
-     * This method is used for performing select operations
+     * This method is used to handle select operations
      *
-     * @param queryString
+     * @param inputString
      */
-    public static void performSelectOperation(String queryString) {
+    public static void handleSelectOperation(String inputString) {
         String tableName = "";
-        List<String> columnNames = new ArrayList<>();
+        List<String> columnList = new ArrayList<>();
 
-        // Get table and column names for the select
-        ArrayList<String> tableTokens = new ArrayList<>(Arrays.asList(queryString.split(" ")));
+        // Get table and column names for the selection operation
+        ArrayList<String> tableTokenList = new ArrayList<>(Arrays.asList(inputString.split(" ")));
         int i;
 
-        for (i = 1; i < tableTokens.size(); i++) {
-            if (tableTokens.get(i).equals("from")) {
+        for (i = 1; i < tableTokenList.size(); i++) {
+            if (tableTokenList.get(i).equals("from")) {
                 ++i;
-                tableName = tableTokens.get(i);
+                tableName = tableTokenList.get(i);
                 break;
             }
-            if (!tableTokens.get(i).equals("*") && !tableTokens.get(i).equals(",")) {
-                if (tableTokens.get(i).contains(",")) {
-                    ArrayList<String> colList = new ArrayList<>(
-                            Arrays.asList(tableTokens.get(i).split(",")));
-                    for (String col : colList) {
-                        columnNames.add(col.trim());
+            if (!tableTokenList.get(i).equals("*") && !tableTokenList.get(i).equals(",")) {
+                if (tableTokenList.get(i).contains(",")) {
+                    ArrayList<String> columns = new ArrayList<>(
+                            Arrays.asList(tableTokenList.get(i).split(",")));
+                    for (String columnRecord : columns) {
+                        columnList.add(columnRecord.trim());
                     }
                 } else
-                    columnNames.add(tableTokens.get(i));
+                    columnList.add(tableTokenList.get(i));
             }
         }
 
         TableInfoHandler tableMetaData = new TableInfoHandler(tableName);
         if (!tableMetaData.tableExists) {
-            System.out.println("Table does not exist!");
+            System.out.println("Table does not exist.");
             return;
         }
 
-        if (!tableMetaData.columnExists(columnNames)) {
-            System.out.println("Invalid column name(s)!");
+        if (!tableMetaData.columnExists(columnList)) {
+            System.out.println("Invalid column name(s).");
             return;
         }
 
         WhereConditionProcessor condition;
         try {
 
-            condition = getConditionFromCurrentQuery(tableMetaData, queryString);
+            condition = getConditionFromCurrentQuery(tableMetaData, inputString);
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
             return;
         }
 
-        if (columnNames.size() == 0) {
-            columnNames = tableMetaData.columnNamesList;
+        if (columnList.size() == 0) {
+            columnList = tableMetaData.columnNamesList;
         }
         try {
 
             RandomAccessFile tableFile = new RandomAccessFile(getTableFilePath(tableName), "r");
             DBOperationsProcessor tableBinaryFile = new DBOperationsProcessor(tableFile);
-            tableBinaryFile.selectRecordsFromTable(tableMetaData, columnNames, condition);
+            tableBinaryFile.selectRecordsFromTable(tableMetaData, columnList, condition);
             tableFile.close();
         } catch (IOException e) {
-            LOGGER.log(Level.SEVERE, "Exception in retrieving the columns", e);
-            System.out.println("! Error selecting columns from table");
+            logger.log(Level.SEVERE, "Exception while retrieving the columns", e);
+            System.out.println("Error while selecting columns from table");
         }
 
     }
